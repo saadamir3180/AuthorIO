ea8b377bf79c569fb11b49da3acdc052c2e211b6
Saad Amir <saadamir318.com>
2023-12-12 00:10:31 +0500

    modified search bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import '../styles/Svg.css'
-import '../styles/Home.css'
-import Circle from './../components/Circle';
-import ZigZag from './../components/ZigZag';
-import { motion } from 'framer-motion';
-import SearchBooks from '../components/SearchBooks';
-
+import React from "react";
+import "../styles/Svg.css";
+import "../styles/Home.css";
+import Circle from "./../components/Circle";
+import ZigZag from "./../components/ZigZag";
+import { motion } from "framer-motion";
+import SearchBooks from "../components/SearchBooks";
+import Dots from "../components/Dots";
 
 const Home = () => {
   return (
-    <motion.div className="box"
-    initial={{opacity:0}}
-    animate={{opacity:1}}
-    exit={{opacity:0}}
+    <motion.div
+      className="box"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
     >
-    <div className='homeContainer'>
-        <h1 className='homeh1'> AI Empowered <br/>Authorship Redefined</h1>
-        <SearchBooks/>
+      <div className="homeContainer">
+        <h1 className="homeh1">
+          {" "}
+          AI Empowered <br />
+          Authorship Redefined
+        </h1>
+        <SearchBooks />
 
-      <div className='svg svgZigZag'><ZigZag/></div>
-      
-      <div className='svg svgCircle'><Circle/></div>
-
-    </div>
+        <div className="svg svgZigZag">
+          <ZigZag />
+        </div>
+        <div className="svg svgCircle">
+          <Circle />
+        </div>
+        <div className="svg svgDots shadow">
+          <Dots />
+        </div>
+      </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
